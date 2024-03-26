9154ae6113e56f513e933f1d1500df1fefe71734
RitaM5 <sporshia108514orva@gmail.com>
2024-03-26 13:17:55 +0600

    theme page popup function changed

--- Js/theme.js
+++ Js/theme.js
@@ -12,18 +12,3 @@ close_btns.forEach(function (btn) {
 	});
 });
 
-//for all category select
-let optionMenu = document.querySelector('.select-menu');
-let selectBtn = optionMenu.querySelector('.select-btn');
-let options = optionMenu.querySelectorAll('.options .option');
-let sBtn_text = optionMenu.querySelector('.sBtn-text');
-
-selectBtn.addEventListener('click', () => optionMenu.classList.toggle('active'));
-
-options.forEach(option => {
-    option.addEventListener('click', () => {
-        let selectedOption = option.querySelector('.option-text').innerText;
-        sBtn_text.value = selectedOption;
-        optionMenu.classList.remove('active');
-    });
-});
