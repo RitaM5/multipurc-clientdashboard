5138b27422f51aca805253828c45b4220291d6cc
RitaM5 <sporshia108514orva@gmail.com>
2024-04-01 22:43:39 +0600

    role selected input file updated

--- Js/staffRole.js
+++ Js/staffRole.js
@@ -5,9 +5,28 @@
 let profilePic = document.getElementById('profile-pic');
 let inputFile = document.getElementById('input-file');
 
-inputFile.onchange = function() {
+inputFile.onchange = function () {
     profilePic.src = URL.createObjectURL(inputFile.files[0]);
 }
+// end image upload
+
+// role select
+const roleContainer = document.querySelector('.selects-role');
+const dropdownRole = roleContainer.querySelector('.role-link');
+let selectedBtn = roleContainer.querySelector('.select-btn');
+let roleLinkData = roleContainer.querySelectorAll('.role-link .option');
+let role_item = roleContainer.querySelector('.role-item');
+
+roleContainer.addEventListener('click', function () {
+    dropdownRole.classList.toggle('show');
+})
+roleLinkData.forEach(option => {
+    option.addEventListener('click', () => {
+        let selectedOption = option.querySelector('.option_text').innerText;
+        role_item.value = selectedOption;
+        roleContainer.classList.remove('active');
+    });
+});
 
 // -----------End Add New Staff---------
 
@@ -16,11 +35,30 @@ inputFile.onchange = function() {
 
 // image change
 
-let changeProfilePic = document.getElementById('change-profile-pic');
-let changeInputFile = document.getElementById('change-input-file');
+let changeProfilePic = document.getElementById('profile-pic');
+let changeInputFile = document.getElementById('input-file');
 
-changeInputFile.onchange = function() {
+changeInputFile.onchange = function () {
     changeProfilePic.src = URL.createObjectURL(inputFile.files[0]);
 }
+// end image change
+
+//edit role select
+const editRoleContainer = document.querySelector('.edit-selects-role');
+const dropdownEditRole = editRoleContainer.querySelector('.edit-role-link');
+let editSelectedBtn = editRoleContainer.querySelector('.select-btn');
+let editRoleLinkData = editRoleContainer.querySelectorAll('.edit-role-link .option');
+let edtRole_item = editRoleContainer.querySelector('.role-item');
+
+editRoleContainer.addEventListener('click', function () {
+    dropdownEditRole.classList.toggle('show');
+})
+editRoleLinkData.forEach(option => {
+    option.addEventListener('click', () => {
+        let editSelectedOption = option.querySelector('.option_text').innerText;
+        edtRole_item.value = editSelectedOption;
+        editRoleContainer.classList.remove('active');
+    });
+});
 
 // -----------End Edit staff---------
